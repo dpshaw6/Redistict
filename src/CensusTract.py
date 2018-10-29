edbd42d3a61227b5abfaa41409ca42df666c2718
Daniel Shaw <dpshaw@gmail.com>
2018-10-28 23:31:02 -0700

    First cut successfully pulling population and boundary data for a state by tracts. It is slow and needs some optimizing.

--- src/CensusTract.py
+++ src/CensusTract.py
@@ -16,31 +16,49 @@
 import requests
 
 class CensusTract:
-    __population = -1
-    __boundary = {}
-    __POPULATION_API_STRING = "https://api.census.gov/data/2010/sf1?get=P0010001&for=tract:*&in=state:"
-    __POPULATION_API_STRING = "https://api.census.gov/data/2010/sf1?get"
-    __POPULATION_API_STRING = "https://api.census.gov/data/2010/sf1?get=P0010001&for=tract:*&in=state:"
+    population = {}
+    boundary = {}
+    geography = {}
+    POPULATION_API_STRING = "https://api.census.gov/data/2010/sf1?get=P0010001&for=tract:*&in=state:"
+    GEOGRAPHY_API_STRING1 = "https://tigerweb.geo.census.gov/arcgis/rest/services/TIGERweb/tigerWMS_Current/MapServer/8/query?"
+    GEOGRAPHY_API_STRING2 = "&text=&objectIds=&time=&geometry=&geometryType=esriGeometryPoint&inSR=&spatialRel=esriSpatialRelIntersects&relationParam=&outFields=TRACT%2CCOUNTY%2CSTATE&returnGeometry=true&returnTrueCurves=false&maxAllowableOffset=&geometryPrecision=&outSR=&returnIdsOnly=false&returnCountOnly=false&orderByFields=&groupByFieldsForStatistics=&outStatistics=&returnZ=false&returnM=false&gdbVersion=&returnDistinctValues=false&resultOffset=&resultRecordCount=&queryByDistance=&returnExtentsOnly=false&datumTransformation=&parameterValues=&rangeValues=&f=json"
     
     def set_population_from_API(self, state_key):
         # Pull data from Census API
-        # https://api.census.gov/data/2010/sf1?get=P0010001&for=tract:*&in=state:02+county:170
-        url = self.__POPULATION_API_STRING+str(state_key)
+        url = self.POPULATION_API_STRING+str(state_key)
         response = requests.get(url)
-        return response.json()
+        population_data = response.json()
+        #[[u'P0010001', u'state', u'county', u'tract'], [u'4476', u'09', u'001', u'010101'], ...
+        for line in population_data[1:]:
+            population = int(line[0])
+            state = line[1]
+            county = line[2]
+            tract = line[3]
+            geokey = state+"_"+county+"_"+tract
+            self.population[geokey] = population
+            self.geography[geokey] = (state,county,tract)
+            
+        return
             
     def get_population(self):
-        return self.__population
+        return self.population
     
     def set_boundary_from_API(self, state_key):
-        # Pull data from Census API
-        population = self.__population
-        pass
+        # Pull data from Census API        
+        # SQL: where=state%3D09+and+county%3D001+and+tract%3D010101
+        for geokey, tract in self.geography.iteritems():
+            sql_where = "where=state%3D"+tract[0]+"+and+county%3D"+tract[1]+"+and+tract%3D"+tract[2]
+            url = self.GEOGRAPHY_API_STRING1+sql_where+self.GEOGRAPHY_API_STRING2
+            response = requests.get(url)
+            polygon = response.json()['features'][0]['geometry']['rings'][0]
+            self.boundary[geokey] = polygon
+
+        return
     
     def get_boundary(self):
-        return self.__boundary
+        return self.boundary
     
     def __init__(self, state_key):
-        self.__population = self.set_population_from_API(state_key)
-        self.__boundary = self.set_boundary_from_API(state_key)
+        self.set_population_from_API(state_key)
+        self.set_boundary_from_API(state_key)
     
--- src/main.py
+++ src/main.py
@@ -1,4 +1,6 @@
 from CensusTract import CensusTract
 
 # States numbers from 01 to 50 (09=Connecticut)
-state_census = CensusTract("09")
+ct_data = CensusTract("09")
+print ct_data.get_population()
+print ct_data.get_boundary()
